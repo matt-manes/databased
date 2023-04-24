3e4c6ad57b965e0aab3c31931544a0a154e20cce
matt manes <mattmanes@pm.me>
2023-04-23 19:39:31 -0500

    Initial commit

---
+++ src/databased/custom_manager.py
@@ -0,0 +1,16 @@
+from databased import DBManager, dbparsers
+
+
+class CustomManager(DBManager):
+    intro = "Starting custommanager (enter help or ? for command info)..."
+    prompt = "custommanager>"
+    dbpath = None  # Replace None with a path to a .db file to set a default database
+
+
+# For help with adding custom functionality see:
+# https://github.com/matt-manes/argshell
+# https://github.com/matt-manes/databased/blob/main/src/databased/dbmanager.py
+# https://github.com/matt-manes/databased/blob/main/src/databased/dbparsers.py
+
+if __name__ == "__main__":
+    CustomManager().cmdloop()
