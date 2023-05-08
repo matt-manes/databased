fcba2c05cd5aa076b534ad276dfec009069246a3
matt manes <mattmanes@pm.me>
2023-05-08 14:53:32 -0500

    chore: build v2.1.0

--- src/databased/__init__.py
+++ src/databased/__init__.py
@@ -1,4 +1,4 @@
 from databased import dbparsers
 
-from .databased import DataBased, _connect, data_to_string, _disconnect
+from .databased import DataBased, _connect, _disconnect, data_to_string
 from .dbshell import DBShell
