eb071bfc7354b97592d50adb4dfc2f9997b2f7c6
matt manes <mattmanes@pm.me>
2023-07-25 17:38:38 -0500

    chore: remove old imports

--- src/databased/__init__.py
+++ src/databased/__init__.py
@@ -1,5 +1,5 @@
 from databased import dbparsers
 
 from .create_shell import create_shell
-from .databased import DataBased, _connect, _disconnect, data_to_string
+from .databased import Databased
 from .dbshell import DBShell
