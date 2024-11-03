d7cd23f828758e4c9335043c3595ad2feef84adb
matt manes <mattmanes@pm.me>
2024-11-02 19:15:30 -0500

    chore: build v4.3.8

--- src/databased/__init__.py
+++ src/databased/__init__.py
@@ -3,5 +3,5 @@ from databased.dbshell import DBShell
 
 from .databased import Databased, Row, Rows
 
-__version__ = "4.3.7"
+__version__ = "4.3.8"
 __all__ = ["Databased", "Row", "Rows", "DBShell", "dbparsers"]
